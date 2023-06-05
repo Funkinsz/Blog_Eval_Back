e17f2a1d5479d82dd24889274d537b097b5b38b9
Funkinsz <fl.crespel@gmail.com>
2023-06-05 23:02:01 +0200

    enfin terminé cette merde

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -6,8 +6,6 @@ router.get("/getMovies", (req, res) => {
   const limit = req.query.limit;
   const user = req.query.user;
 
-  console.log(user);
-
   let sql;
   let idMoviesSet;
   if (limit) {
@@ -18,34 +16,48 @@ router.get("/getMovies", (req, res) => {
   connection.query(sql, (err, result) => {
     if (err) throw err;
 
+    // si un utilisateur est connectée, on génère les likes depuis la table favorite + movies
     if (user) {
+      // d'abord on selectionne les films liké dans la table favorite en lien avec l'idUser
       fav = `SELECT * FROM favorite WHERE idUser = ${user} AND isFav = 1`;
       connection.query(fav, (err, resFav) => {
         if (err) throw err;
 
+        // la boucle for ajoute l'object isFav = 0 pour préparer le tableau Movies et le fusion avec le tableau Favorite
         for (let index = 0; index < result.length; index++) {
           result[index].isFav = 0;
         }
+
+        // on mélange les 2 tableaux
         const fusion = resFav.concat(result);
+
+        // on supprime les doublons
         const array = [...new Set(fusion)];
 
-        resFav.idMovies === result.idMovies
-          ? (result.liked = true)
-          : (result.liked = false);
-
+        // on crée un nouveau tableau qui se base sur l'idMovies (present dans movies et favorite)
         idMoviesSet = new Set();
+        // on map sur ce tableau pour recuperer l'idMovies
         const updatedArray = array.map((obj) => {
+          // .has permet de vérifier si l'idMovies à déjà été appelé
           if (idMoviesSet.has(obj.idMovies)) {
+            // convertit les "isFav: 0" du tableau Mvoies en "isFav: 1" s'ils sont liké dans le tableau favorite
             obj.isFav = 1;
           } else {
+            // si le film n'est pas présent dans la table Favorite, il sera ajouté dans le nouveau tableau
             idMoviesSet.add(obj.idMovies);
           }
           return obj;
         });
 
-        console.log(updatedArray);
-        res.send(JSON.stringify(updatedArray));
+        // la dernière étape consiste a gardé les objects qui ont nameMovies, cela supprime les doublons.
+        // on aurai pu le faire également avec poster, resume, duration...
+        const filterArray = updatedArray.filter((object) =>
+          object.hasOwnProperty("nameMovies")
+        );
+        res.send(JSON.stringify(filterArray));
       });
+
+      // si l'utilisateur n'est pas connectée, on génère les films sans filtre like
     } else {
       console.log("Liste de films récupérés");
       res.send(JSON.stringify(result));
@@ -58,7 +70,6 @@ router.get("/getOneMovie", (req, res) => {
 });
 
 router.post("/deleteMovie", (req, res) => {
-  console.log(req.body);
   let id = req.body.id;
   let sqlDelete = `DELETE FROM movies WHERE idMovies=${id}`;
 
@@ -72,18 +83,17 @@ router.post("/deleteMovie", (req, res) => {
 router.post("/toggleLiked", (req, res) => {
   const idM = req.body.idMovies;
   const idU = req.body.user.idUser;
-  const idF = req.body.isLike.idFav;
 
+  console.log(idM);
+
+  //  on verifie si la table Favorite retourne des valeurs avec l'idUser 
   const verify = `SELECT isFav FROM favorite WHERE idUser = ${idU} AND idMovies = ${idM}`;
 
   connection.query(verify, (err, resultVerify) => {
     if (err) throw err;
 
-    console.log(resultVerify.length);
+    // si le resultat est suppérieur a 1, alors on envoie un update
     if (resultVerify.length > 0) {
-      console.log("resultat du fav : ");
-      console.log(resultVerify[0].isFav);
-
       const liked = resultVerify[0].isFav === 0 ? 1 : 0;
       const update = `UPDATE favorite SET isFav = ${liked} WHERE idMovies = ${idM} AND idUser = ${idU}`;
 
@@ -95,19 +105,18 @@ router.post("/toggleLiked", (req, res) => {
           (err, result) => {
             if (err) throw err;
 
-            console.log("test liked");
-            console.log(result);
+            // génère un true ou false (le contrere de ce qui visible en BDD)
             liked === 0 ? (result[0].liked = false) : (result[0].liked = true);
-
-            console.log(result[0]);
             res.send(JSON.stringify(result[0]));
           }
         );
       });
+
+      // si le resultat est égale a 0, alors on envoie un insert
     } else {
-      console.log("insert");
       const fav = `INSERT INTO favorite (idUser, idMovies, isFav) VALUES(${idU}, ${idM}, 1)`;
 
+      console.log("insert");
       connection.query(fav, (err, resultInsert) => {
         if (err) throw err;
 
@@ -116,9 +125,9 @@ router.post("/toggleLiked", (req, res) => {
           (err, result) => {
             if (err) throw err;
 
-            console.log(result);
-            liked === 0 ? (result[0].liked = false) : (result[0].liked = true);
-            res.send(JSON.stringify(result[0]));
+            const liked = (result[0].isFav = 1);
+
+            res.send(JSON.stringify(liked));
           }
         );
       });
@@ -126,30 +135,13 @@ router.post("/toggleLiked", (req, res) => {
   });
 });
 
-router.get("/getFav", (req, res) => {
-  const idM = req.query.idMovie;
-  const idU = req.query.user;
-
-  connection.query(
-    `SELECT idFav, isFav FROM favorite WHERE idMovies = ${idM} AND idUser = ${idU}`,
-    (err, result) => {
-      if (err) throw err;
-      if (result.length > 0) {
-        res.send(JSON.stringify(result[0]));
-      }
-    }
-  );
-});
-
 router.get("/myFav", (req, res) => {
   const idU = req.query.idUser;
 
-  console.log(idU);
   connection.query(
     `SELECT DISTINCT * FROM favorite INNER JOIN movies WHERE idUser = ${idU} AND isFav = 1 AND favorite.idMovies = movies.idMovies`,
     (err, result) => {
       if (err) throw err;
-      console.log(result);
       res.send(JSON.stringify(result));
     }
   );
