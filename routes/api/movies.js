a6ca89ccb0c9cfa7fbb8a24c05c2dee8a77b86c0
Funkinsz <fl.crespel@gmail.com>
2023-06-05 16:29:40 +0200

    ça me saoul

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -4,22 +4,52 @@ const router = require("express").Router();
 
 router.get("/getMovies", (req, res) => {
   const limit = req.query.limit;
-  const user = req.query.user
+  const user = req.query.user;
+
+  console.log(user);
+
   let sql;
-  console.log("test");
+  let idMoviesSet;
   if (limit) {
-    if (user) {
-      sql = `SELECT * FROM movies ORDER BY created_at ASC LIMIT ${limit}`;
-    } else {
-      sql = `SELECT * FROM movies ORDER BY created_at ASC LIMIT ${limit}`;
-    }
+    sql = `SELECT * FROM movies ORDER BY created_at ASC LIMIT ${limit}`;
   } else {
     sql = `SELECT COUNT(*) as count FROM movies`;
   }
   connection.query(sql, (err, result) => {
     if (err) throw err;
-    console.log("Liste de films récupérés");
-    res.send(JSON.stringify(result));
+
+    if (user) {
+      fav = `SELECT * FROM favorite WHERE idUser = ${user} AND isFav = 1`;
+      connection.query(fav, (err, resFav) => {
+        if (err) throw err;
+
+        for (let index = 0; index < result.length; index++) {
+          result[index].isFav = 0;
+        }
+        const fusion = resFav.concat(result);
+        const array = [...new Set(fusion)];
+
+        resFav.idMovies === result.idMovies
+          ? (result.liked = true)
+          : (result.liked = false);
+
+        idMoviesSet = new Set();
+        const updatedArray = array.map((obj) => {
+          if (idMoviesSet.has(obj.idMovies)) {
+            obj.isFav = 1;
+          } else {
+            idMoviesSet.add(obj.idMovies);
+          }
+          return obj;
+        });
+
+        console.log(updatedArray);
+        res.send(JSON.stringify(updatedArray));
+      });
+    } else {
+      console.log("Liste de films récupérés");
+      res.send(JSON.stringify(result));
+    }
   });
 });
 
@@ -42,32 +72,34 @@ router.post("/deleteMovie", (req, res) => {
 router.post("/toggleLiked", (req, res) => {
   const idM = req.body.idMovies;
   const idU = req.body.user.idUser;
+  const idF = req.body.isLike.idFav;
 
-  const verify = `SELECT * FROM favorite WHERE idUser = ${idU} AND idMovies = ${idM}`;
+  const verify = `SELECT isFav FROM favorite WHERE idUser = ${idU} AND idMovies = ${idM}`;
 
   connection.query(verify, (err, resultVerify) => {
     if (err) throw err;
 
     console.log(resultVerify.length);
     if (resultVerify.length > 0) {
-      console.log("2222");
+      console.log("resultat du fav : ");
+      console.log(resultVerify[0].isFav);
 
-      const liked = resultVerify === true ? "1" : "0";
-      const update = `UPDATE favorite SET isFav = "${liked}" WHERE idMovies = ${idM} AND idUser = ${idU}`;
+      const liked = resultVerify[0].isFav === 0 ? 1 : 0;
+      const update = `UPDATE favorite SET isFav = ${liked} WHERE idMovies = ${idM} AND idUser = ${idU}`;
 
       connection.query(update, (err, result) => {
         if (err) throw err;
         console.log("Film toggle en base de données");
         connection.query(
-          `SELECT * FROM favorite WHERE idMovies = ${idM} AND idUser = ${idU}`,
+          `SELECT * FROM movies WHERE idMovies = ${idM}`,
           (err, result) => {
             if (err) throw err;
-            console.log("3333");
 
+            console.log("test liked");
             console.log(result);
-            result[0].liked == 0
-              ? (result[0].liked = false)
-              : (result[0].liked = true);
+            liked === 0 ? (result[0].liked = false) : (result[0].liked = true);
+
+            console.log(result[0]);
             res.send(JSON.stringify(result[0]));
           }
         );
@@ -80,15 +112,12 @@ router.post("/toggleLiked", (req, res) => {
         if (err) throw err;
 
         connection.query(
-          `SELECT * FROM favorite WHERE idMovies = ${idM} AND idUser = ${idU}`,
+          `SELECT * FROM movies WHERE idMovies = ${idM}`,
           (err, result) => {
             if (err) throw err;
-            console.log("3333");
 
             console.log(result);
-            result[0].liked == 0
-              ? (result[0].liked = false)
-              : (result[0].liked = true);
+            liked === 0 ? (result[0].liked = false) : (result[0].liked = true);
             res.send(JSON.stringify(result[0]));
           }
         );
@@ -97,19 +126,31 @@ router.post("/toggleLiked", (req, res) => {
   });
 });
 
-router.get("/isFav", (req, res) => {
-  const idM = req.query.movie;
+router.get("/getFav", (req, res) => {
+  const idM = req.query.idMovie;
   const idU = req.query.user;
 
-  console.log(idM);
-
   connection.query(
-    `SELECT idFav FROM favorite WHERE idMovies = ${idM} AND idUser = ${idU}`,
+    `SELECT idFav, isFav FROM favorite WHERE idMovies = ${idM} AND idUser = ${idU}`,
     (err, result) => {
       if (err) throw err;
       if (result.length > 0) {
-        res.send(JSON.stringify(result));
+        res.send(JSON.stringify(result[0]));
       }
+    }
+  );
+});
+
+router.get("/myFav", (req, res) => {
+  const idU = req.query.idUser;
+
+  console.log(idU);
+  connection.query(
+    `SELECT DISTINCT * FROM favorite INNER JOIN movies WHERE idUser = ${idU} AND isFav = 1 AND favorite.idMovies = movies.idMovies`,
+    (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(JSON.stringify(result));
     }
   );
 });
